3721cc51c576aa66ff7c29901c3a2d05a953fa7d
Elizaveta <elizaveta.lebedeva@bolt.eu>
2019-11-23 15:11:13 +0200

    added image saving

--- junction/viz_denoised.py
+++ junction/viz_denoised.py
@@ -11,6 +11,7 @@ import time
 def return_15_fps(frames):
     i = 0
     averages = np.zeros(shape=(1, 19800))
+    averages = np.zeros(shape=(1, 19800))
     for batch in range(1, frames.shape[0] + 1):
         if batch % 100 == 0:
             i_rows = i * 100
@@ -31,15 +32,15 @@ data_folder = "data/"
 rolling = 5
 min_amplitude = 100
 
-office_data = np.loadtxt(data_folder + 'demo_short.txt', delimiter=',')
+office_data = np.loadtxt(data_folder + 'demo.txt', delimiter=',')
 print(office_data.shape)
 # office_data = return_15_fps(office_data)
 print(office_data.shape)
-df = pd.DataFrame(office_data)
-print("original df shape" + str(df.shape))
+# df = pd.DataFrame(office_data)
+# print("original df shape" + str(df.shape))
 df_ma = pd.DataFrame(office_data).rolling(rolling).mean()
 print("rolling df shape" + str(df_ma.shape))
-# office_data = np.reshape((df.loc[rolling:,:]-df_ma.loc[rolling:,:]).to_numpy(),(df.loc[rolling:,:].shape[0],180,110)) # remove avg images - no background noize kind
+# office_data_diff = np.reshape((df.loc[rolling:,:]-df_ma.loc[rolling:,:]).to_numpy(),(df.loc[rolling:,:].shape[0],180,110)) # remove avg images - no background noize kind
 office_data = np.reshape(office_data, (office_data.shape[0], 180, 110))
 
 def report_people(number):
@@ -66,21 +67,32 @@ print(office_data.shape)
 matplotlib.use('TkAgg')
 ims = []
 fig = plt.figure()
-for i in range(rolling, office_data.shape[0]):
+for i in range(rolling, rolling+1):
     # im = plt.imshow(np.clip(abs(office_data[i, :, :]-office_data[i-1, :, :]), a_min=0, a_max=None))
     original_frame = office_data[i, :, :]
+    #todo save to file
+    # matplotlib.image.imsave('orignal_1.png', original_frame)
+
+    # frames_diff = office_data_diff[i, :, :]
+    # matplotlib.image.imsave('frames_diff_1.png', frames_diff)
+    #todo save
+
     frame_transposed_flipped = np.flip(np.transpose(original_frame))
-    details_removed = frame_transposed_flipped
+    # matplotlib.image.imsave('frame_transposed_flipped_1.png', frame_transposed_flipped)
+    #todo save to file
+    # details_removed = frame_transposed_flipped
     details_removed = np.clip(frame_transposed_flipped, a_min=frame_transposed_flipped.max()-15, a_max=None) #to remove not intense pixels
+    #todo save
+    # matplotlib.image.imsave('clean_1.png', details_removed)
     im = plt.imshow(details_removed)
     ims.append([im])
 
     if i % 10 == 0:
         b = sum(pd.DataFrame(original_frame).max().diff() / pd.DataFrame(original_frame).max() > 0.13)
-        report_people(b)
+        # report_people(b)
         print('Detected humans:', b)
         time.sleep(1)
 
-ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True, repeat=False)
+ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True, repeat=True)
 # repeat_delay=1000)
 plt.show()
